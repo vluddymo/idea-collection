04d6149f7dda95fc6806e9b2adf2a0ca8a3c795c
Wladi Moser <wladi.moser@gmail.com>
2020-06-25 17:55:00 +0200

    Fix attempt No.4

--- project-planning/src/main/java/de/neuefische/projectplanning/service/IdeaService.java
+++ project-planning/src/main/java/de/neuefische/projectplanning/service/IdeaService.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import de.neuefische.projectplanning.model.Idea;
 
-import java.util.List;
-
 @Service
 public class IdeaService {
     private final IdeaMongoDb ideaDb;
--- project-planning/src/main/java/de/neuefische/projectplanning/utils/IdUtils.java
+++ project-planning/src/main/java/de/neuefische/projectplanning/utils/IdUtils.java
@@ -1,7 +1,6 @@
 package de.neuefische.projectplanning.utils;
 
 import org.springframework.stereotype.Service;
-
 import java.util.UUID;
 
 @Service
